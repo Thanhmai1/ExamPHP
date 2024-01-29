c21635dc1f823d4a4177a8f8d44110c7fe81b7c8
Thanhmai1 <141890766+Thanhmai1@users.noreply.github.com>
2024-01-29 09:49:08 +0700

    Khác
    Exam

---
+++ Exam/resources/views/profile.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Profile</title>
+    </head>
+<body>
+    <h1>Welcome Profile Page</h1>
+<h2>YOUR NAME {{$name}}</h2>
+</body>
+</html>
